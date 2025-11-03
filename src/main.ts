a1387a3a774038b739ec0392b065a481a99825d0
ranggaju <ranggajzp@gmail.com>
2025-11-03 14:27:16 +0700

    feat: mini-project-api init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
