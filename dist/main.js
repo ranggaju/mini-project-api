23d020d749c65df23b5679be7f80e18d57b62acd
Awang Syahsiah <82008890+spaceman1705@users.noreply.github.com>
2025-11-27 02:21:09 +0700

    F2 1 1 (#26)
    * memulai F2-1
    
    * buat tabel database dengan prisma
    
    * updt-verif-login
    
    * fixing error register verify
    
    * saya commit takut lupa kalo ada yang baru
    
    * dashboard dan admin BE
    
    * update admin dan dashboard
    
    * fixing create event error
    
    * typo correction
    
    * fixing error, dashboard org dan admin
    
    * fixing build 2
    
    * fixing aaaaaa
    
    * vercel api
    
    * tambah folder api
    
    * rubah isi tsconfig
    
    * fix: downgrade nanoid to v3.3.7

--- dist/main.js
+++ dist/main.js
@@ -6,10 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
-const env_config_1 = require("./config/env.config");
 const error_middleware_1 = __importDefault(require("./middlewares/error.middleware"));
 const routers_1 = __importDefault(require("./routers"));
-const routers_2 = __importDefault(require("./routers"));
+const profile_route_1 = __importDefault(require("./routers/profile.route"));
 const dashboard_route_1 = __importDefault(require("./routers/dashboard.route"));
 const admin_route_1 = __importDefault(require("./routers/admin.route"));
 const transaction_route_1 = __importDefault(require("./routers/transaction.route"));
@@ -18,7 +17,7 @@ app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/api", routers_1.default);
-app.use("/api/profile", routers_2.default);
+app.use("/api/profile", profile_route_1.default);
 app.use("/api/dashboard", dashboard_route_1.default);
 app.use("/api/admin", (req, res, next) => {
     console.log("🔵 /api/admin/* request:", req.method, req.url);
@@ -26,6 +25,4 @@ app.use("/api/admin", (req, res, next) => {
 }, admin_route_1.default);
 app.use('/api/transactions', transaction_route_1.default);
 app.use(error_middleware_1.default);
-app.listen(env_config_1.PORT, () => {
-    console.log(`Server is running on port ${env_config_1.PORT}`);
-});
+exports.default = (req, res) => app(req, res);
